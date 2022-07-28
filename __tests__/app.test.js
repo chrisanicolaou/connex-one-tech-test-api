dca9aa664c100e4d4fb5056d9417043e8543b829
Christos Nicolaou <chrisanicolaou>
2022-07-28 17:48:14 +0100

    Adds middleware to check for correct token in authorization header

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
-const { app } = require('../app');
+const { app, authHeader } = require('../app');
 
-describe('GET: /api', () => {
-  test('200: Returns with working message', () =>
+describe('GET: /api/test', () => {
+  test('200: Provided the correct authorisation token, returns working message', () =>
     request(app)
       .get('/api/test')
+      .set({ Authorization: authHeader })
       .expect(200)
       .then((result) => {
         expect(result.body).toEqual(
@@ -13,4 +14,29 @@ describe('GET: /api', () => {
           }),
         );
       }));
+  test('203: When missing authorisation token, returns error: no authorization credentials set', () =>
+    request(app)
+      .get('/api/test')
+      .expect(403)
+      .then((result) => {
+        expect(result.body).toEqual(
+          expect.objectContaining({
+            error: 'No authorization credentials set!',
+          }),
+        );
+      }));
+  test('203: When providing an incorrect auth token, returns error: Incorrect authorization credentials', () => {
+    const fakeAuth = { Authorization: 'fakeToken' };
+    return request(app)
+      .get('/api/test')
+      .set(fakeAuth)
+      .expect(403)
+      .then((result) => {
+        expect(result.body).toEqual(
+          expect.objectContaining({
+            error: 'Incorrect authorization credentials!',
+          }),
+        );
+      });
+  });
 });
--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const { getTest } = require('./controllers/index');
+require('dotenv').config();
 
 const app = express();
-const cors = require('cors');
-
-const { getTest } = require('./controllers/index');
+const authHeader = process.env.MY_SECRET_TOKEN;
 
 app.use(cors());
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).send({ error: 'No authorization credentials set!' });
+  }
+  if (req.headers.authorization !== authHeader) {
+    return res
+      .status(403)
+      .send({ error: 'Incorrect authorization credentials!' });
+  }
+  next();
+});
+
 app.get('/api/test', getTest);
 
-module.exports = { app };
+module.exports = { app, authHeader };
